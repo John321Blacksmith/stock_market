9b68ced71867fe445faa7dc627bde0bf7d2f192e
John321Blacksmith <kuzmichevi321@yandex.ru>
2023-06-29 23:27:13 +0300

    changed constants

--- backend/market/models.py
+++ backend/market/models.py
@@ -1,9 +1,11 @@
 import random
 from django.db import models
 from django.contrib.auth.models import User
-from . import options
+from .options import EXCHANGES
 # Create your models here.
 
+
+INSTRUMENTS = [(str(index), EXCHANGES[index]) for index in range(1, len(EXCHANGES))]
 
 class Order(models.Model):
 	"""
@@ -17,7 +19,7 @@ class Order(models.Model):
 	status = models.CharField(default='Active', max_length=25)
 	price = models.DecimalField(max_digits=14, decimal_places=5)
 	amount = models.DecimalField(max_digits=11, decimal_places=2)
-	instrument = models.CharField(choices=options.INSTRUMENTS, max_length=9)
+	instrument = models.CharField(choices=INSTRUMENTS, max_length=9)
 
 	class Meta:
 		abstract = True
