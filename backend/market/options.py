8dcd167765fda5b44f33e0996daf28b36687c5f1
John321Blacksmith <kuzmichevi321@yandex.ru>
2023-06-29 22:43:09 +0300

    first steps

---
+++ backend/market/options.py
@@ -0,0 +1,14 @@
+INSTRUMENTS = [
+
+(1, 'USD/CAD'),
+(2, 'EUR/USD'),
+(3, 'USD/CHF'),
+(4, 'GBP/USD'), 
+(5, 'NZD/USD'),
+(6, 'AUD/USD'), 
+(7, 'USD/JPY'),
+(8, 'EUR/CAD'), 
+(9, 'EUR/AUD'),
+(10, 'EUR/AUD'),
+
+]
