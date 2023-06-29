e4b21ede9909038b8cc04eae3d142a609591d185
John321Blacksmith <kuzmichevi321@yandex.ru>
2023-06-29 23:59:52 +0300

    the User model has been extended to sell() & buy() actions

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,31 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+class CustomUser(User):
+	"""
+	The traditional User model
+	has been extended so
+	every User has either 
+	buy(), or sell() method.
+	"""
+
+
+	# the behavioral methods 
+	# below take the data from
+	# the JSON serializers
+	def buy(self, *args, **kwargs):
+		"""
+		Perform a buying action.
+		:returns: <Purchase obj>.
+		"""
+		return 
+
+	def sell(self, *args, **kwargs):
+		"""
+		Perform a selling
+		action.
+		:returns: <Sale obj>
+		"""
+		return 
