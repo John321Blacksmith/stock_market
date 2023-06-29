bb41960beff8610327bcdd58371c9177e1cb84e2
John321Blacksmith <kuzmichevi321@yandex.ru>
2023-06-29 22:40:37 +0300

    абстрактный класс Order и дочерние Purchase, Sale интегрированы

---
+++ backend/market/models.py
@@ -0,0 +1,44 @@
+import random
+from django.db import models
+from django.contrib.auth.models import User
+from . import options
+# Create your models here.
+
+
+class Order(models.Model):
+	"""
+	This model represents
+	every order wether it
+	is a purchase or sale.
+	"""
+
+	time_created = models.DateTimeField(auto_now_add=True)
+	time_changed = models.DateTimeField(auto_now_add=True)
+	status = models.CharField(default='Active', max_length=25)
+	price = models.DecimalField(max_digits=14, decimal_places=5)
+	amount = models.DecimalField(max_digits=11, decimal_places=2)
+	instrument = models.CharField(choices=options.INSTRUMENTS, max_length=9)
+
+	class Meta:
+		abstract = True
+
+	def return_side(self):
+		"""
+		This method returns a
+		one of two options the
+		user does.
+		"""
+		return self.__class__
+
+
+class Purchase(Order):
+	"""
+	This model represents
+	every purchase.
+	"""
+
+class Sale(Order):
+	"""
+	This model represents
+	every sal.
+	"""
