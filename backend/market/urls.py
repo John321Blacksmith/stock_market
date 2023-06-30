364474ebaa05ce142812b888bfba350dc52974db
John321Blacksmith <kuzmichevi321@yandex.ru>
2023-06-30 22:24:16 +0300

    added endpoints

---
+++ backend/market/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+	path('recent-orders/', views.RecentOrdersList.as_view()),
+	path('active-orders/', views.ActiveOrdersList.as_view()),
+	path('active-orders/<int:pk>/', views.ActiveOrderDetail.as_view()),
+	path('purchases/', views.PurchaseList.as_view()),
+	path('sales/', views.SalesList.as_view()),
+]
